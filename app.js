194403c41197a44ebbf258f2905ac0acdfbebdcf
amber-lynx <261199713+amber-lynx@users.noreply.github.com>
2026-03-11 21:02:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 let combo=[]
-let damage=0
-let frame=0
+
+const iconMap={
+
+"弱P":"icons/LP.png",
+"中P":"icons/MP.png",
+"強P":"icons/HP.png",
+
+"弱K":"icons/LK.png",
+"中K":"icons/MK.png",
+"強K":"icons/HK.png"
+
+}
+
+function getIcon(name){
+
+for(let key in iconMap){
+
+if(name.includes(key))
+return iconMap[key]
+
+}
+
+return null
+
+}
 
 function convertCommand(cmd){
 
@@ -9,20 +32,6 @@ return cmd
 .replaceAll("214","↓↙←")
 .replaceAll("66","→→")
 .replaceAll("22","↓↓")
-
-}
-
-function formatMove(name,cmd,od){
-
-let text=name
-
-if(cmd)
-text+="【"+convertCommand(cmd)+"】"
-
-if(od)
-text+=" (OD"+od+")"
-
-return text
 
 }
 
@@ -42,32 +51,34 @@ standing:[
 
 special:[
 
-{name:formatMove("武神旋風脚","214K",2)},
+{name:"武神旋風脚【214K】"},
+{name:"武神旋風脚（OD）【214KK】"},
 
-{name:formatMove("空中武神旋風脚（前J）","214K",2)},
+{name:"空中武神旋風脚（前J）【214K】"},
+{name:"空中武神旋風脚（前JOD）【214KK】"},
 
-{name:"疾駆け",followups:[
+{name:"流転一文字【236P】"},
+{name:"流転一文字（OD）【236PP】"},
+
+{name:"彩隠形【214P】"},
+{name:"彩隠形（OD）【214PP】"},
+
+{name:"荒鵺捻り（前J）【236P】"},
+{name:"荒鵺捻り（前JOD）【236PP】"},
+
+{name:"疾駆け【214K】",
+followups:[
 
 {name:"急停止 P"},
 {name:"胴刎ね 弱K"},
 {name:"影すくい 中K"},
 {name:"首狩り 強K"},
-{name:"弧空",branch:true}
+{name:"弧空"}
 
 ]},
 
-{name:"弧空",followups:[
-
-{name:"武神イズナ落とし P"},
-{name:"武神鉾刃脚 K"}
-
-]},
-
-{name:formatMove("流転一文字","236P",2)},
-
-{name:formatMove("彩隠形","214P",2)},
-
-{name:"召雷細工",followups:[
+{name:"召雷細工",
+followups:[
 
 {name:"細工手裏剣 ↓↓P"},
 {name:"乱れ細工手裏剣 ↓↓PP"}
@@ -76,11 +87,50 @@ special:[
 
 ],
 
+unique:[
+
+{name:"水切り蹴り【3中K】"},
+{name:"風車【4強K】"},
+
+{name:"飛箭蹴【6強K】",
+followups:[
+
+{name:"↖"},
+{name:"↑"},
+{name:"↗"}
+
+]},
+
+{name:"肘落とし（前J）【2中P】"},
+
+{name:"武神虎連牙 中P＞強P"},
+{name:"武神天架拳 弱P＞中P＞強P＞強K"},
+{name:"武神獄鎖拳 弱P＞中P＞2強P＞強K"},
+{name:"武神獄鎖投げ 弱P＞中P＞2強P＞2強K"}
+
+],
+
+throw:[
+
+{name:"縄掛背負い【5or6＋弱P弱K】"},
+{name:"鍾打巴【4＋弱P弱K】"}
+
+],
+
+system:[
+
+{name:"ドライブインパクト【強P強K】"},
+{name:"ドライブリバーサル【強P強K】"},
+{name:"パリィ【中P中K】"},
+{name:"ドライブラッシュ【66】"}
+
+],
+
 sa:[
 
-{name:formatMove("武神乱拍手 SA1","236236K")},
-{name:formatMove("武神天翔亢竜 SA2","214214P")},
-{name:formatMove("武神顕現神楽 SA3","236236P")}
+{name:"武神乱拍手 SA1【236236K】"},
+{name:"武神天翔亢竜 SA2【214214P】"},
+{name:"武神顕現神楽 SA3【236236P】"}
 
 ]
 
@@ -90,53 +140,62 @@ function drawMoves(){
 
 let html=""
 
-html+="<div class='category'><h2>Standing</h2><div class='standingGrid'>"
-
-moves.standing.forEach(m=>{
-
-html+=`
-
-<button class="standingBtn" onclick='addMove(${JSON.stringify(m)})'>
-<img src="icons/LP.png">
-</button>
-
-`
-
-})
-
-html+="</div></div>"
-
-html+="<div class='category'><h2>Special</h2>"
-
-moves.special.forEach(m=>{
-
-html+=button(m)
-
-})
-
-html+="</div>"
-
-html+="<div class='category'><h2>SA</h2>"
-
-moves.sa.forEach(m=>{
-
-html+=button(m)
-
-})
-
-html+="</div>"
+html+=drawCategory("Standing",moves.standing,true)
+html+=drawCategory("Special",moves.special)
+html+=drawCategory("特殊技",moves.unique)
+html+=drawCategory("通常投げ",moves.throw)
+html+=drawCategory("共通システム",moves.system)
+html+=drawCategory("SA",moves.sa)
 
 document.getElementById("moves").innerHTML=html
 
 }
 
-function button(m){
+function drawCategory(title,list,grid=false){
 
-return `
-<button onclick='addMove(${JSON.stringify(m)})'>
-${m.name}
+let html=`<div class="category"><h3>${title}</h3>`
+
+if(grid){
+
+html+=`<div class="standingGrid">`
+
+list.forEach(m=>{
+
+let icon=getIcon(m.name)
+
+html+=`
+<button class="standingBtn" onclick='addMove(${JSON.stringify(m)})'>
+<img src="${icon}">
 </button>
 `
+
+})
+
+html+=`</div>`
+
+}else{
+
+list.forEach(m=>{
+
+let icon=getIcon(m.name)
+let img=""
+
+if(icon)
+img=`<img class="icon" src="${icon}">`
+
+html+=`
+<button onclick='addMove(${JSON.stringify(m)})'>
+${img}${m.name}
+</button>
+`
+
+})
+
+}
+
+html+="</div>"
+
+return html
 
 }
 
@@ -157,27 +216,11 @@ let html=""
 
 list.forEach(f=>{
 
-if(f.branch){
-
-html+=`<button onclick="branch('弧空')">弧空</button>`
-
-}else{
-
 html+=`<button onclick='addMove(${JSON.stringify(f)})'>${f.name}</button>`
-
-}
 
 })
 
 document.getElementById("followups").innerHTML=html
-
-}
-
-function branch(name){
-
-let m=moves.special.find(v=>v.name==name)
-
-showFollow(m.followups)
 
 }
 
@@ -187,7 +230,13 @@ let html=""
 
 combo.forEach((m,i)=>{
 
-html+=`<span class="comboMove">${m.name}</span>`
+let icon=getIcon(m.name)
+let img=""
+
+if(icon)
+img=`<img class="icon" src="${icon}">`
+
+html+=`<span class="comboMove">${img}${m.name}</span>`
 
 if(i<combo.length-1)
 html+=`<span class="arrow">→</span>`
