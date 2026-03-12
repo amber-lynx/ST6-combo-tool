485affa8c595fde78cf9cb90a100624ce39e665e
amber-lynx <261199713+amber-lynx@users.noreply.github.com>
2026-03-13 00:48:00 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,139 +1,71 @@
-let combo=[]
-let commandData
+let combo = []
 
-fetch("commands.json")
-.then(res=>res.json())
-.then(data=>{
-commandData=data
-renderCommands()
+async function loadCommands(){
+
+const res = await fetch("commands.json")
+
+const data = await res.json()
+
+createButtons(data.normal,"normal")
+createButtons(data.unique,"unique")
+createButtons(data.special,"special")
+createButtons(data.follow,"follow")
+createButtons(data.sa,"sa")
+createButtons(data.system,"system")
+createButtons(data.throw,"throw")
+
+}
+
+function createButtons(list,id){
+
+const area = document.getElementById(id)
+
+if(!area) return
+
+list.forEach(move=>{
+
+const btn = document.createElement("button")
+
+btn.textContent = move.name
+
+btn.onclick = ()=>addMove(move.name)
+
+area.appendChild(btn)
+
 })
 
-function renderCommands(){
-
-const container=document.getElementById("command-list")
-container.innerHTML=""
-
-for(const key in commandData.categories){
-
-const category=commandData.categories[key]
-
-const section=document.createElement("div")
-section.className="command-section"
-section.id=key
-
-const title=document.createElement("h3")
-title.innerText=category.title
-
-section.appendChild(title)
-
-category.moves.forEach(move=>{
-section.appendChild(createMove(move))
-})
-
-container.appendChild(section)
-
 }
 
-}
-
-
-function createMove(move){
-
-const box=document.createElement("div")
-
-const btn=document.createElement("button")
-btn.innerText=move.name
-
-btn.onclick=()=>addCombo(move)
-
-box.appendChild(btn)
-
-if(move.followups){
-
-const follow=document.createElement("div")
-follow.className="followup"
-
-move.followups.forEach(f=>{
-follow.appendChild(createMove(f))
-})
-
-box.appendChild(follow)
-
-}
-
-return box
-
-}
-
-
-function addCombo(move){
+function addMove(move){
 
 combo.push(move)
+
 renderCombo()
 
 }
-
 
 function renderCombo(){
 
-const comboBox=document.getElementById("combo")
-const damageBox=document.getElementById("damage")
+const box = document.getElementById("combo")
 
-comboBox.innerHTML=""
-
-let total=0
-
-combo.forEach(move=>{
-
-const span=document.createElement("span")
-span.innerText=move.name+" ＞ "
-
-comboBox.appendChild(span)
-
-if(move.damage){
-total+=move.damage
-}
-
-})
-
-damageBox.innerText="合計ダメージ: "+total
+box.innerHTML = combo.join(" ＞ ")
 
 }
 
-
-function removeLast(){
+function undo(){
 
 combo.pop()
+
 renderCombo()
 
 }
 
+function clearCombo(){
 
-function removeAll(){
+combo = []
 
-combo=[]
 renderCombo()
 
 }
 
-
-function copyCombo(){
-
-let text=""
-
-combo.forEach(move=>{
-text+=move.name+" ＞ "
-})
-
-navigator.clipboard.writeText(text)
-
-alert("コンボコピーしました")
-
-}
-
-
-function changeBg(num){
-
-document.body.className="bg"+num
-
-}
+loadCommands()
